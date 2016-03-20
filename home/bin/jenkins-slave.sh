ca7a496b016595059780642ce55594967c6336c1
Bjørn Åge Tungesvik <bjorn.tungesvik@gmail.com>
2016-03-20 14:58:09 +0100

    Fix

--- home/bin/jenkins-slave.sh
+++ home/bin/jenkins-slave.sh
@@ -7,6 +7,7 @@ export JENKINS_HOME=$HOME
 generate_passwd_file `id -u` `id -g`
 
 master_username=${JENKINS_USERNAME:-"admin"}
+master_password=${JENKINS_PASSWORD:-"password"}
 slave_executors=${EXECUTORS:-"1"}
 
 if [[ $# -lt 1 ]] || [[ "$1" == "-"* ]]; then
@@ -15,7 +16,7 @@ if [[ $# -lt 1 ]] || [[ "$1" == "-"* ]]; then
   JAR=`ls -1 /opt/jenkins-slave/bin/swarm-client-*.jar | tail -n 1`
 
   if [[ "$@" != *"-master "* ]] && [ ! -z "$JENKINS_PORT_8080_TCP_ADDR" ]; then
-	PARAMS="-master http://$JENKINS_SERVICE_HOST:$JENKINS_SERVICE_PORT -tunnel $JENKINS_SLAVE_SERVICE_HOST:$JENKINS_SLAVE_SERVICE_PORT -username ${master_username} -passwordEnvVariable MASTER_PASSWORD -executors ${slave_executors}"
+	PARAMS="-master http://$JENKINS_SERVICE_HOST:$JENKINS_SERVICE_PORT -tunnel $JENKINS_SLAVE_SERVICE_HOST:$JENKINS_SLAVE_SERVICE_PORT -username ${master_username} -password ${master_password} -executors ${slave_executors}"
   fi
 
   echo Running java $JAVA_OPTS -jar $JAR -fsroot $HOME $PARAMS "$@"
